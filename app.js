2e65c581ee17d690177316d17d264337e2192d0a
mjjsingh <jyoti1536@gmail.com>
2024-02-06 11:57:10 +0530

    limiting middleware execution to post requests

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const express =  require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/',(req, res, next) => {
-    console.log('This always runs!');
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-app.use('/add-product',(req, res, next)=> {
-    console.log('i m another middleware!');
-    res.send('<h1>The "Add Product" page</h1>');
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
-app.use('/',(req, res, next)=> {
-    console.log('i m another middleware!');
-    res.send('<h1>Hello from express!</h1>');
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
 });
 
 app.listen(3000);
