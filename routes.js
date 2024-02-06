0549b18fa4a83a67ebae619972708e40aad6d5ef
mjjsingh <jyoti1536@gmail.com>
2024-02-06 10:29:28 +0530

    Restarting the Debugger automatically after editing my app

--- routes.js
+++ routes.js
@@ -5,13 +5,14 @@ const requestHandler = (req, res) => {
   const method = req.method;
   if (url === '/') {
     res.write('<html>');
-    res.write('<head><title>Enter Message</title><head>');
+    res.write('<head><title>Enter Message</title></head>');
     res.write(
       '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
     );
     res.write('</html>');
     return res.end();
   }
+  
   if (url === '/message' && method === 'POST') {
     const body = [];
     req.on('data', chunk => {
@@ -30,7 +31,7 @@ const requestHandler = (req, res) => {
   }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
-  res.write('<head><title>My First Page</title><head>');
+  res.write('<head><title>My First Page</title></head>');
   res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
   res.write('</html>');
   res.end();
